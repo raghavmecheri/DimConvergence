82508f7148df5cf0b5fef4e7307a8a9a5109715e
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-12-09 00:18:45 +0530

    Added experiment run files

---
+++ exp2.py
@@ -0,0 +1,31 @@
+import pickle
+
+from framework.grid import Grid
+from framework.experiment import ExperimentTwo
+
+from multiprocessing import Pool
+
+PROCESSES = 10
+
+def _save(obj, name):
+	with open(name, "wb") as f:
+		pickle.dump(obj, f)
+
+grid = {
+"size": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+"sampling": ["random", "stratified"],
+"convergence": ["none"],
+"dataset": ["mnist", "fmnist", "olivetti"],
+"algorithm": ["umap", "tsne"]
+}
+
+def _gs(filename):
+	gs = Grid(grid, ExperimentTwo)
+	results = gs.run()
+	results_json = [x.fetch() for x in results]
+	_save(results_json, filename)
+
+def main():
+	targets = ["data/experimenttwo_{}.pkl".format(str(x+1)) for x in range(0, PROCESSES)]
+	pool = Pool(PROCESSES)
+	pool.map(_gs, targets)
---
+++ run.sh
@@ -0,0 +1,4 @@
+rm -rf data/
+mkdir data/
+nohup python -u exp1.py &
+nohup python -u exp2.py &
