e6b75806094eb4376cb64f315bbad24c3d536f18
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-12-09 17:55:23 +0530

    4x speed boost

--- exp2.py
+++ exp2.py
@@ -5,6 +5,8 @@ import pickle
 
 from multiprocessing import Pool
 
+CONVERGENCE = ["none"]
+
 PROCESSES = 10
 
 def _save(obj, name):
@@ -14,17 +16,15 @@ def _save(obj, name):
 grid = {
 "size": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
 "sampling": ["random", "stratified"],
-"convergence": ["none"],
 "dataset": ["mnist", "fmnist", "olivetti"],
 "algorithm": ["umap", "tsne"]
 }
 
 def _gs(filename):
 	print("Running ExperimentTwo pointing to: {}".format(str(filename)))
-	gs = Grid(grid, ExperimentTwo)
+	gs = Grid(grid, CONVERGENCE, ExperimentTwo, progress=False)
 	results = gs.run()
-	results_json = [x.fetch() for x in results]
-	_save(results_json, filename)
+	_save(results, filename)
 
 def main():
 	targets = ["data/experimenttwo_{}.pkl".format(str(x+1)) for x in range(0, PROCESSES)]
